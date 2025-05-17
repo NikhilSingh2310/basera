fa5bde616d8ae4c534a431c9fe2ed9e702d4d237
Nikhil Singh <thakurnikhilsingh2004@gmail.com>
2025-05-17 14:06:18 +0530

    first commit

---
+++ src/main/java/com/basera/baserabackend/BaseraApplication.java
@@ -0,0 +1,13 @@
+package com.basera.baserabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basera/baserabackend/BaseraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basera.baserabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
