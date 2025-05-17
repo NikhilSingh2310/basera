0bb52e81159ba72c8647384949266b08b243232d
Nikhil Singh <thakurnikhilsingh2004@gmail.com>
2025-05-17 14:10:28 +0530

    Create User Entity and UserRole Enum

--- src/main/java/com/basera/baserabackend/BaseraApplication.java
+++ src/main/java/com/basera/baserabackend/BaseraApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BaseraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaseraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BaseraApplication.class, args);
+    }
 
 }
